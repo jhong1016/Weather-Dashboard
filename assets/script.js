a9cdd92bff651d18682b47374252a5336ede96d3
Jin Hong <jnhg1016@gmail.com>
2021-04-02 01:03:53 +1100

    fuction to display current conditions on open weather maps added

--- assets/script.js
+++ assets/script.js
@@ -7,18 +7,14 @@ function currentWeather(){
     navigator.geolocation.getCurrentPosition(function (position){
         longitude = position.coords.longitude;
         latitude = position.coords.latitude;
-    
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" +  APIkey;
-
     $.ajax ({
         url: queryURL,
         method: "GET"
     })
-
     // We store all of the retrieved data inside of an object called "response"
     .then(function(response) {
     var iconCode = response.weather[0].icon;
-    
     var iconurl = "http://openweathermap.org/img/w/" + iconCode + ".png";
     $(".city").html("<h1> " + response.name + " </h1>");
     $(".temp").text("Temperature: " + response.main.temp + " ºC");
@@ -26,15 +22,12 @@ function currentWeather(){
     $(".wind").text("Wind Speed: " + response.wind.speed + " MPH");
     $("#wicon").attr("src", iconurl);
   });
-
   });
 };
 
 currentWeather();
 
-////////////////////////
-
-// Error handler for fetch,
+// Error handler for fetch
 var handleErrors = (response) => {
     if (!response.ok) {
         throw Error(response.statusText);
@@ -42,3 +35,68 @@ var handleErrors = (response) => {
     return response;
 }
 
+// Function to get and display the current conditions on Open Weather Maps
+var getCurrentConditions = (event) => {
+    // Obtain city name from the search box
+    let city = $('#search-city').val();
+    currentCity= $('#search-city').val();
+    // Set the queryURL to fetch from API using weather search - added units=imperial to fix
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + APIkey;
+    fetch(queryURL)
+    .then(handleErrors)
+    .then((response) => {
+        return response.json();
+    })
+    .then((response) => {
+        // Save city to local storage
+        saveCity(city);
+        $('#search-error').text("");
+        // Create icon for the current weather using Open Weather Maps
+        let currentWeatherIcon="https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+        // Offset UTC timezone - using moment.js
+        let currentTimeUTC = response.dt;
+        let currentTimeZoneOffset = response.timezone;
+        let currentTimeZoneOffsetHours = currentTimeZoneOffset / 60 / 60;
+        let currentMoment = moment.unix(currentTimeUTC).utc().utcOffset(currentTimeZoneOffsetHours);
+        // Render cities list
+        renderCities();
+        // Obtain the 5 day forecast for the searched city
+        getFiveDayForecast(event);
+        // Set the header text to the found city name
+        $('#header-text').text(response.name);
+        // HTML for the results of search
+        let currentWeatherHTML = `
+            <h3>${response.name} ${currentMoment.format("DD/MM/YYYY)")}<img src="${currentWeatherIcon}"></h3>
+            <ul class="list-unstyled">
+                <li>Temperature: ${response.main.temp}&#8457;</li>
+                <li>Humidity: ${response.main.humidity}%</li>
+                <li>Wind Speed: ${response.wind.speed} mph</li>
+                <li id="uvIndex">UV Index:</li>
+            </ul>`;
+        // Append the results to the DOM
+        $('#current-weather').html(currentWeatherHTML);
+        // Get the latitude and longitude for the UV search from Open Weather Maps API
+        let latitude = response.coord.lat;
+        let longitude = response.coord.lon;
+        let uvQueryURL = "api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&APPID=" + owmAPI;
+        // API solution for Cross-origin resource sharing (CORS)
+        uvQueryURL = "https://cors-anywhere.herokuapp.com/" + uvQueryURL;
+        // Fetch the UV information and build the color display for the UV index
+        fetch(uvQueryURL)
+        .then(handleErrors)
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            let uvIndex = response.value;
+            $('#uvIndex').html(`UV Index: <span id="uvVal"> ${uvIndex}</span>`);
+            if (uvIndex>=0 && uvIndex<3){
+                $('#uvVal').attr("class", "uv-favorable");
+            } else if (uvIndex>=3 && uvIndex<8){
+                $('#uvVal').attr("class", "uv-moderate");
+            } else if (uvIndex>=8){
+                $('#uvVal').attr("class", "uv-severe");
+            }
+        });
+    })
+}
