a52403943198897f06f61d4d514fab077fe4ad1a
Jin Hong <jnhg1016@gmail.com>
2021-04-02 16:50:30 +1100

    function to obtain 5 day forecast added

--- assets/script.js
+++ assets/script.js
@@ -3,30 +3,6 @@ var APIkey = "311ad2f7f09a1df9a71b2b60e7af05cc";
 var currentCity = "";
 var lastCity = "";
 
-function currentWeather(){
-    navigator.geolocation.getCurrentPosition(function (position){
-        longitude = position.coords.longitude;
-        latitude = position.coords.latitude;
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIkey;
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-    // We store all of the retrieved data inside of an object called "response"
-    .then(function(response) {
-    var iconCode = response.weather[0].icon;
-    var iconurl = "http://openweathermap.org/img/w/" + iconCode + ".png";
-    $(".city").html("<h1> " + response.name + " </h1>");
-    $(".temp").text("Temperature: " + response.main.temp + " ºC");
-    $(".humidity").text("Humidity: " + response.main.humidity + " %");
-    $(".wind").text("Wind Speed: " + response.wind.speed + " MPH");
-    $("#wicon").attr("src", iconurl);
-  });
-  });
-};
-
-currentWeather();
-
 // Error handler for fetch
 var handleErrors = (response) => {
     if (!response.ok) {
@@ -40,8 +16,8 @@ var getCurrentConditions = (event) => {
     // Obtain city name from the search box
     let city = $('#search-city').val();
     currentCity = $('#search-city').val();
-    // Set the queryURL to fetch from API using weather search - added units=imperial to fix
-    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + APIkey;
+    // Set the queryURL to fetch from API using weather search
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial"+"&appid="+APIkey;
     fetch(queryURL)
     .then(handleErrors)
     .then((response) => {
@@ -52,7 +28,7 @@ var getCurrentConditions = (event) => {
         saveCity(city);
         $('#search-error').text("");
         // Create icon for the current weather using Open Weather Maps
-        let currentWeatherIcon = "api.openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+        let currentWeatherIcon = "http://openweathermap.org/img/w/"+response.weather[0].icon+".png";
         // Offset UTC timezone - using moment.js
         let currentTimeUTC = response.dt;
         let currentTimeZoneOffset = response.timezone;
@@ -66,7 +42,7 @@ var getCurrentConditions = (event) => {
         $('#header-text').text(response.name);
         // HTML for the results of search
         let currentWeatherHTML = `
-            <h3>${response.name} ${currentMoment.format("DD/MM/YYYY)")}<img src="${currentWeatherIcon}"></h3>
+            <h3>${response.name} ${currentMoment.format("DD/MM/YYYY")}<img src="${currentWeatherIcon}"></h3>
             <ul class="list-unstyled">
                 <li>Temperature: ${response.main.temp}&#8457;</li>
                 <li>Humidity: ${response.main.humidity}%</li>
@@ -78,9 +54,9 @@ var getCurrentConditions = (event) => {
         // Get the latitude and longitude for the UV search from Open Weather Maps API
         let latitude = response.coord.lat;
         let longitude = response.coord.lon;
-        let uvQueryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&APPID=" + owmAPI;
+        let uvQueryURL = "https://api.openweathermap.org/data/2.5/uvi?lat="+latitude+"&lon="+longitude+"&APPID="+APIkey;
         // API solution for Cross-origin resource sharing (CORS)
-        uvQueryURL = "https://cors-anywhere.herokuapp.com/" + uvQueryURL;
+        uvQueryURL = "https://cors-anywhere.herokuapp.com/"+uvQueryURL;
         // Fetch the UV information and build the color display for the UV index
         fetch(uvQueryURL)
         .then(handleErrors)
@@ -98,5 +74,50 @@ var getCurrentConditions = (event) => {
                 $('#uvVal').attr("class", "uv-severe");
             }
         });
+    })
+}
+
+// Function to obtain the five day forecast and display to HTML
+var getFiveDayForecast = (event) => {
+    let city = $('#search-city').val();
+    // Set up URL for API search using forecast search
+    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&units=imperial"+"&APPID="+APIkey;
+    // Fetch from API
+    fetch(queryURL)
+        .then (handleErrors)
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+        // HTML template
+        let fiveDayForecastHTML = `
+        <h2>5-Day Forecast:</h2>
+        <div id="fiveDayForecastUl" class="d-inline-flex flex-wrap">`;
+        // Loop over the 5 day forecast and build the template HTML using UTC offset and Open Weather Map icon
+        for (let i = 0; i < response.list.length; i++) {
+            let dayData = response.list[i];
+            let dayTimeUTC = dayData.dt;
+            let timeZoneOffset = response.city.timezone;
+            let timeZoneOffsetHours = timeZoneOffset / 60 / 60;
+            let thisMoment = moment.unix(dayTimeUTC).utc().utcOffset(timeZoneOffsetHours);
+            let iconURL = "https://openweathermap.org/img/w/"+dayData.weather[0].icon+".png";
+            // Only displaying mid-day forecasts
+            if (thisMoment.format("HH:mm:ss") === "11:00:00" || thisMoment.format("HH:mm:ss") === "12:00:00" || thisMoment.format("HH:mm:ss") === "13:00:00") {
+                fiveDayForecastHTML += `
+                <div class="weather-card card m-2 p0">
+                    <ul class="list-unstyled p-3">
+                        <li>${thisMoment.format("MM/DD/YY")}</li>
+                        <li class="weather-icon"><img src="${iconURL}"></li>
+                        <li>Temp: ${dayData.main.temp}&#8457;</li>
+                        <br>
+                        <li>Humidity: ${dayData.main.humidity}%</li>
+                    </ul>
+                </div>`;
+            }
+        }
+        // Build the HTML template
+        fiveDayForecastHTML += `</div>`;
+        // Append the five-day forecast to the DOM
+        $('#five-day-forecast').html(fiveDayForecastHTML);
     })
 }
