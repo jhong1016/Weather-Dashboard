a164ee61c04f5fdf74e4dfc012cd3b78667a6a7a
Jin Hong <jnhg1016@gmail.com>
2021-04-02 18:00:07 +1100

    5 day forecase function added

--- assets/script.js
+++ assets/script.js
@@ -27,9 +27,12 @@ var weatherRequest = function (city) {
     if (!city) {
         return;
     };
-    var weatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=311ad2f7f09a1df9a71b2b60e7af05cc";
+    
+    var weatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=e2f667cde55a60ea38271c0834d19b9e";
+    
     // Fetch the response
     fetch(weatherApi)
+
     .then(function (response) {
         if (!response || !response.ok) {
             throw new Error('There was an error!');
@@ -76,10 +79,65 @@ var weatherRequest = function (city) {
         uvIndexContainer.classList = "secondary-text uv-class";
         cityDetailsDiv.appendChild(uvIndexContainer);
 
-        // Set uvValue
+        // Set UV Value
         var uvValue = uvResponse.value;
         uvIndexEl.innerHTML = "UV Index: ";
         uvValueDisplay.setAttribute("id", "uv-index");
         uvValueDisplay.innerHTML = uvValue;
         uvIndexContainer.appendChild(uvIndexEl);
         uvIndexContainer.appendChild(uvValueDisplay);
+
+        if (uvResponse.value > 7) {
+            document.querySelector("#uv-index").classList = "uv-result rounded bg-danger";
+        } else if (uvResponse.value >= 2 && uvResponse.value <= 7) {
+            document.querySelector("#uv-index").classList = "uv-result rounded bg-warning";
+        } else if (uvResponse.value <= 2) {
+            document.querySelector("#uv-index").classList = "uv-result rounded bg-success";
+        }
+        return fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + uvResponse.lat + "&lon=" + uvResponse.lon + "&appid=e2f667cde55a60ea38271c0834d19b9e&units=imperial");
+    })
+
+    .then(function (forecastResponse) {
+        return forecastResponse.json();
+    })
+
+    .then(function (forecastResponse) {
+        // For loop to display 5 day forecast
+        for (var i = 1; i < 6; i++) {
+            var forecastEl = document.createElement("div");
+            forecastEl.classList = "forecast-card card-body rounded-lg border-dark bg-info text-light";
+            forecastContainer.appendChild(forecastEl);
+
+            // Display date 
+            var dateDiv = document.createElement("div");
+            dateDiv.classList = "secondary-text card-title";
+            var forecastDate = moment.utc(forecastResponse.daily[i].dt * 1000).format("dddd, MMM DD");
+            dateDiv.innerHTML = "<h5 class='font-weight-bold'>" + forecastDate + "</h5>";
+            forecastEl.appendChild(dateDiv);
+
+            // Weather icon
+            var iconDiv = document.createElement("div");
+            iconDiv.innerHTML = "<img src='http://openweathermap.org/img/w/" + forecastResponse.daily[i].weather[0].icon + ".png' class='forecast-icon' alt=Current weather icon/>";
+            forecastEl.appendChild(iconDiv);
+
+            // Display day temperature forecast
+            var tempDiv = document.createElement("div");
+            tempDiv.classList = "card-text secondary-text";
+            tempDiv.innerHTML = "<h6>Day Temp:<span>" + " " + Math.round(forecastResponse.daily[i].temp.day) + "&#176F</span></h6>" + "<h6>Night Temp:<span>" + " " + Math.round(forecastResponse.daily[i].temp.night) + " &#176F</span></h6>";
+            forecastEl.appendChild(tempDiv);
+
+            // Display humidity forecast
+            var humidDiv = document.createElement("div");
+            humidDiv.classList = "card-text secondary-text";
+            humidDiv.innerHTML = "<h6>Humidity:<span>" + " " + forecastResponse.daily[i].humidity + "%</span></h6>";
+            forecastEl.appendChild(humidDiv);
+        }
+    })
+    
+    .catch(function (error) {
+        removePrevious();
+        alert(error.message);
+        document.querySelector("#search-bar").value = "";
+        return;
+    });
+};
